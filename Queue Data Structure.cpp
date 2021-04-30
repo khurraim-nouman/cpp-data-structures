661ff46936890b79d525db5ad81cd6d280461f08
Khurraim <khurraim@khurraim.com>
2021-05-01 04:50:57 +0500

    Queue Data Structure Program

--- Queue Data Structure.cpp
+++ Queue Data Structure.cpp
@@ -67,9 +67,9 @@ class Queue
 			else if(front == rear)
 			{
 				x = arr[front];
+				arr[front] = 0;
 				rear = -1;
 				front = -1;
-				arr[front] = 0;
 				return x;
 			}
 			else
@@ -87,10 +87,83 @@ class Queue
 			return (rear - front + 1);
 		}
 		
+		void display()
+		{
+			cout << "All the values in the Queue are : " << endl;
+			for (int i = 0; i < 5; i++)
+			{
+				cout << arr[i] << "  ";
+			}
+		}
+		
 		
 };
 
 int main()
 {
+	Queue q1;
+	int option, value;
 	
+	do
+	{
+		cout << "\n \n Which Operation Do You Want To Perform? Select Option Number. Enter 0 To Exit. " << endl;
+		cout << "1. Enqueue() " << endl;
+		cout << "2. Dequeue() " << endl;
+		cout << "3. isEmpty() " << endl;
+		cout << "4. isFull() " << endl;
+		cout << "5. count() " << endl;
+		cout << "6. display() " << endl;
+		cout << "7. Clear Screen  " << endl << endl;
+		
+		cin >> option;
+		
+		switch(option)
+		{
+			case 0:
+				break;
+				
+			case 1:
+				cout << "Enqueue Operation \nEnter an item to Enqueue in the Queue " << endl;
+				cin >> value;
+				q1.enqueue(value);
+				break;
+				
+			case 2:
+				cout << "Dequeue Operation \nDequeued Value : " << q1.dequeue() << endl;
+				break;
+				
+			case 3:
+				if (q1.isEmpty())
+					cout << "Queue is Empty " << endl;
+				else
+					cout << "Queue is not Empty " << endl;
+				break;
+				
+			case 4:
+				if (q1.isFull())
+					cout << "Queue is Full " << endl;
+				else
+					cout << "Queue is not Full " << endl;
+				break;				
+				
+				
+			case 5:
+				cout << "Count Operation. \n Count Of items in Queue " << q1.count() << endl;
+				break;
+				
+			case 6:
+				cout << "Display Function Called - " << endl;
+				q1.display();
+				break;
+				
+			case 7:
+				system("cls");
+				break;	
+				
+			default:
+				cout << "Enter Proper Number "	<< endl;
+				break;
+		}
+		
+	} while (option != 0);
 }
